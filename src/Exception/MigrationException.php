fcd3db62103481f57eaba885c9e28f2192e670b0
Samet TEMIZER <nestisamet@gmail.com>
2020-07-06 09:57:42 +0300

    phpcs fix

--- src/Exception/MigrationException.php
+++ src/Exception/MigrationException.php
@@ -2,6 +2,15 @@
 
 namespace YD\Exception;
 
+/**
+ * Class MigrationException
+ *
+ * @category YD
+ * @package  YD\Exception
+ * @author   Samet TEMIZER <nestisamet@gmail.com>
+ * @license  https://github.com/nestisamet/php-migration/blob/master/LICENSE MIT
+ * @link     https://stemizer.com
+ */
 class MigrationException extends \Exception
 {
     protected $msg;
@@ -10,9 +19,10 @@ class MigrationException extends \Exception
 
     /**
      * MigrationException constructor.
-     * @param array $msg
-     * @param int $code
-     * @param int $responseCode
+     *
+     * @param array $msg          msg
+     * @param int   $code         code
+     * @param int   $responseCode status code
      */
     public function __construct(array $msg, $code = 0,  $responseCode = 200)
     {
@@ -21,16 +31,31 @@ class MigrationException extends \Exception
         $this->code = $code;
     }
 
+    /**
+     * Desc
+     *
+     * @return array
+     */
     public function getMsg()
     {
         return $this->msg;
     }
 
+    /**
+     * Desc
+     *
+     * @return int
+     */
     public function getCustomCode()
     {
         return $this->code;
     }
 
+    /**
+     * Desc 
+     * 
+     * @return int
+     */
     public function getResponseCode()
     {
         return $this->responseCode;
--- src/Migration.php
+++ src/Migration.php
@@ -5,52 +5,76 @@ namespace YD;
 use YD\Exception\MigrationException;
 use PDO;
 
-class Migration {
+/**
+ * Class Migration
+ *
+ * @category YD
+ * @package  YD
+ * @author   Samet TEMIZER <nestisamet@gmail.com>
+ * @license  MIT License
+ * @link     https://stemizer.com
+ */
+class Migration
+{
 
     protected $dbh;
     protected $scriptsPath;
 
     /**
      * Migration constructor.
-     * @param PDO $pdoObj pdo-instance
+     *
+     * @param PDO $pdoObj      pdo-instance
      * @param $scriptsPath sql-files
-     * @throws MigrationException
      */
-    public function __construct(PDO $pdoObj, $scriptsPath) {
+    public function __construct(PDO $pdoObj, $scriptsPath)
+    {
         $this->scriptsPath = $scriptsPath;
         $this->dbh = $pdoObj;
         $this->dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $this->validateArguments();
-        $this->requirements();
+        $this->_validateArguments();
+        $this->_requirements();
     }
 
     /**
+     * Desc
+     *
      * @throws MigrationException
+     * @return null
      */
-    private function validateArguments() {
+    private function _validateArguments()
+    {
         if (!$this->dbh instanceof PDO) {
-            throw new MigrationException([
+            throw new MigrationException(
+                [
                 'error' => 'argument-I not a pdo instance',
-            ]);
+                ]
+            );
         }
         if (!is_dir($this->scriptsPath)) {
-            throw new MigrationException([
+            throw new MigrationException(
+                [
                 'error' => 'argument-II is not a directory',
-            ]);
+                ]
+            );
         }
     }
 
     /**
-     * execute migration scripts
+     * Execute migration scripts
+     *
+     * @return null
      */
-    public function run() {
+    public function run()
+    {
         $last = $this->dbh->query("select `i` from `_migration` order by `i` desc")
-                          ->fetch();
+            ->fetch();
         $migration_last = ($last) ? $last['i'] : -1;
         $migrations_path = $this->scriptsPath;
         $migrations = [];
         foreach (scandir($migrations_path) as $file) {
-            if (in_array($file, ['.', '..'])) continue;
+            if (in_array($file, ['.', '..'])) {
+                continue;
+            }
             $migrations[] = $file;
         }
         if (!($sizeof=sizeof($migrations)) || $migration_last >= $sizeof-1) {
@@ -66,16 +90,20 @@ class Migration {
                 if (!($statement = file_get_contents("$migrations_path/$script"))) {
                     continue;
                 }
-                if ($this->dbh->query($statement) === FALSE) {
-                    throw new MigrationException([
+                if ($this->dbh->query($statement) === false) {
+                    throw new MigrationException(
+                        [
                         'script' => $script,
                         'error' => $this->dbh->errorInfo()
-                    ]);
+                        ]
+                    );
                 }
-                $this->dbh->exec("
+                $this->dbh->exec(
+                    "
                     insert into `_migration`(`i`, `migration`)
                     values ($i, '{$script}')
-                ");
+                "
+                );
                 $this->dbh->commit();
             }
             catch (MigrationException $e) {
@@ -91,22 +119,26 @@ class Migration {
                 $this->dbh->rollBack();
                 // todo: log unexpected
             }
-        }
-        while ($i < $sizeof-1);
+        } while ($i < $sizeof-1);
     }
 
     /**
-     * structural requirements
+     * Structural requirements
+     * 
+     * @return null
      */
-    private function requirements() {
+    private function _requirements()
+    {
         $sth = $this->dbh->query('SHOW TABLES LIKE "_migration"');
         if (!$sth->rowCount()) {
-            $this->dbh->query('
+            $this->dbh->query(
+                '
                 CREATE TABLE `_migration` (
                     `i` SMALLINT NOT NULL,
                     `migration` VARCHAR(100) NOT NULL
                 );
-            ');
+            '
+            );
         }
     }
 }
